b7dfdde87ae8a2dc38fc603647ab5820e2b38841
David Hull <david.hull37@gmail.com>
2018-03-16 11:45:44 -0500

    Added Code for Background Slide Show

--- project1.js
+++ project1.js
@@ -241,3 +241,20 @@ function checkboxFilter() {
     update_location(activity);
     
 }
+
+
+// Below is JavaScript Code for Slideshow
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
