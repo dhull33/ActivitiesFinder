819a163d7161d73ac3ffb30b259d84a8dfa6d610
Adrian Jablonski <Jablonski.Adrian1@gmail.com>
2018-03-19 14:09:53 -0500

    Merging files with search bar above images

--- scripts/project1.js
+++ scripts/project1.js
@@ -325,5 +325,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 4000); // Change image every 2 seconds
 }
