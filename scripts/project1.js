17c1aa4b43945049acf80900b938b05918204bdc
David Hull <david.hull37@gmail.com>
2018-03-19 22:24:24 -0500

    Delete Carousel Function

--- scripts/project1.js
+++ scripts/project1.js
@@ -316,21 +316,3 @@ function checkboxFilter() {
     update_weather(activity);
     
 }
-
-
-/*// Below is JavaScript Code for Slideshow
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 4000); // Change image every 2 seconds
-}
-*/
